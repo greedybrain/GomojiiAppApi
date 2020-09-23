4fc52cd812fe1b3d58e9a575fbc864fe7601a80b
WhatYouCodingBoutWillis <whatyoucodingboutwillis@WhatYouCodingBoutWilliss-MacBook-Pro.local>
2020-09-23 10:22:11 -0400

    added logged_in helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::API
         skip_before_action :verify_authenticity_token
-        helper_method :current_user
+        helper_method :current_user, :logged_in
 
         def current_user 
                 @current_user ||= User.find(session[:user_id]) if session[:user_id]
+        end
+
+        def logged_in 
+                !!current_user
         end
 
 end
