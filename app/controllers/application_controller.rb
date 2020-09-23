3865c1826fe0cf69a398694644a4ec3f94b7845f
WhatYouCodingBoutWillis <whatyoucodingboutwillis@WhatYouCodingBoutWilliss-MacBook-Pro.local>
2020-09-23 10:04:47 -0400

    created helper method > current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+        skip_before_action :verify_authenticity_token
+        helper_method :current_user
+
+        def current_user 
+                @current_user ||= User.find(session[:user_id]) if session[:user_id]
+        end
+
 end
