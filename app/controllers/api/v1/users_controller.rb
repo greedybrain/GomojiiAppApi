9747221a65cd08aac61a5b8fa681b9a684ca36c8
WhatYouCodingBoutWillis <whatyoucodingboutwillis@WhatYouCodingBoutWilliss-MacBook-Pro.local>
2020-09-23 10:36:18 -0400

    serialized info from index and show action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,10 +2,20 @@ class Api::V1::UsersController < ApplicationController
 
         def index 
                 users = User.all.order('created_at DESC')
+                if users 
+                        render json: {
+                                users: UserSerializer.new(users).serializable_hash
+                        }
+                end
         end
 
         def show 
                 user = User.find(params[:id])
-        end
+                if user 
+                        render json: {
+                                user: UserSerializer.new(user).serializable_hash
+                        }
+                end
+        end 
 
 end
