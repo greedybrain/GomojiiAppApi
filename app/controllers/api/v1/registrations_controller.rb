4335a7059f0584d2f4cec770b923bc9333aef01e
WhatYouCodingBoutWillis <whatyoucodingboutwillis@WhatYouCodingBoutWilliss-MacBook-Pro.local>
2020-09-23 23:41:10 -0400

    created registrations ctrllr, added signup action

---
+++ app/controllers/api/v1/registrations_controller.rb
@@ -0,0 +1,28 @@
+class Api::V1::RegistrationsController < ApplicationController
+
+        def signup 
+                user = User.new(user_params)
+                if user.save
+                        session[:user_id] = user.id
+                        render json: {
+                                user: UserSerializer.new(user),
+                                status: :created,
+                                logged_in: true
+                        }
+                else
+                        render json: {
+                                email_error: user.errors.messages[:email],
+                                password_error: user.errors.messages[:password],
+                                password_confirmation_error: user.errors.messages[:password_confirmation],
+                                status: 400
+                        }
+                end
+        end
+
+        private 
+
+        def user_params 
+                params.permit(:email, :password, :password_confirmation)
+        end
+
+end
