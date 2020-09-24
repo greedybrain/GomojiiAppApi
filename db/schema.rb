377048633d3545c2f4f65ae0e76cf4be395afcc4
WhatYouCodingBoutWillis <whatyoucodingboutwillis@WhatYouCodingBoutWilliss-MacBook-Pro.local>
2020-09-24 17:12:30 -0400

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_24_024607) do
+ActiveRecord::Schema.define(version: 2020_09_24_195911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "emojis", force: :cascade do |t|
+    t.string "slug"
+    t.string "character"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "user_favorites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "emoji_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["emoji_id"], name: "index_user_favorites_on_emoji_id"
+    t.index ["user_id"], name: "index_user_favorites_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -22,4 +38,6 @@ ActiveRecord::Schema.define(version: 2020_09_24_024607) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "user_favorites", "emojis"
+  add_foreign_key "user_favorites", "users"
 end
