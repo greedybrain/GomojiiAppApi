2edbb82e7874043b0c0e3d33f56b81b2356b79dd
WhatYouCodingBoutWillis <whatyoucodingboutwillis@WhatYouCodingBoutWilliss-MacBook-Pro.local>
2020-09-24 17:13:01 -0400

    created user favorites schema

---
+++ db/migrate/20200924195911_create_user_favorites.rb
@@ -0,0 +1,10 @@
+class CreateUserFavorites < ActiveRecord::Migration[6.0]
+  def change
+    create_table :user_favorites do |t|
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :emoji, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
