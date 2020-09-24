606689573633c806a5c2e6faadf37ca83e020a24
WhatYouCodingBoutWillis <whatyoucodingboutwillis@WhatYouCodingBoutWilliss-MacBook-Pro.local>
2020-09-24 17:11:37 -0400

    created has_many relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
 
         validates :email, uniqueness: true, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
         validates :password, presence: true, length: { minimum: 8 }
+
+        has_many :user_favorites, dependent: :destroy
+        has_many :emojis, through: :user_favorites, dependent: :destroy
 end
