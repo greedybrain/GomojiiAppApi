cbfc24f1a4df59a33cf2892bd7d8f27e0d626dea
WhatYouCodingBoutWillis <whatyoucodingboutwillis@WhatYouCodingBoutWilliss-MacBook-Pro.local>
2020-09-24 17:12:38 -0400

    created seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,16 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+#a
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+naya = User.create!(email: "willisnaya@gmail.com", password: "password", password_confirmation: "password")
+marissa = User.create!(email: "mjoseph@gmail.com", password: "password", password_confirmation: "password")
+emoji1 = Emoji.create!(slug: "victory-hand", character: "✌️")
+emoji2 = Emoji.create!(slug: "vulcan-salute", character: "🖖")
+
+naya.emojis << emoji1
+naya.emojis << emoji2
+marissa.emojis << emoji1
+marissa.emojis << emoji2
