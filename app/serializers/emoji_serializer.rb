68c35ea8f15b8ce7e68b342ff853113ceb7487a2
WhatYouCodingBoutWillis <whatyoucodingboutwillis@WhatYouCodingBoutWilliss-MacBook-Pro.local>
2020-10-08 08:11:48 -0400

    added attrs to emoji serializer

--- app/serializers/emoji_serializer.rb
+++ app/serializers/emoji_serializer.rb
@@ -1,4 +1,4 @@
 class EmojiSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :slug, :character, :user_favorites  
+  attributes :slug, :character, :unicodeName, :codePoint, :group, :subGroup, :variants, :user_favorites  
 end
