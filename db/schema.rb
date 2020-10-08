e9a753d33bddb3087e0c974bdd5adfb228ca10ad
WhatYouCodingBoutWillis <whatyoucodingboutwillis@WhatYouCodingBoutWilliss-MacBook-Pro.local>
2020-10-08 08:11:57 -0400

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_24_195911) do
+ActiveRecord::Schema.define(version: 2020_10_07_131547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(version: 2020_09_24_195911) do
     t.string "character"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "unicodeName"
+    t.string "codePoint"
+    t.string "group"
+    t.string "subGroup"
+    t.text "variants"
   end
 
   create_table "user_favorites", force: :cascade do |t|
