c956d491d032771fa7e44cf415114c092eb3cf2e
WhatYouCodingBoutWillis <whatyoucodingboutwillis@WhatYouCodingBoutWilliss-MacBook-Pro.local>
2020-10-08 08:11:32 -0400

    added array attribute to emojis

--- app/models/emoji.rb
+++ app/models/emoji.rb
@@ -1,4 +1,6 @@
 class Emoji < ApplicationRecord
+        serialize :variants, Array
+
         has_many :user_favorites, dependent: :destroy
         has_many :users, through: :user_favorites, dependent: :destroy
 end
