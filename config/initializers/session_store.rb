88a37a5ff874a19c2eb882e3d8b2549be4c1e9d4
WhatYouCodingBoutWillis <whatyoucodingboutwillis@WhatYouCodingBoutWilliss-MacBook-Pro.local>
2020-10-19 19:36:23 -0400

    ...

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
-if Rails.env == "production"
-        Rails.application.config.session_store :cookie_store, key: '_gomojii', domain: 'https://gomojii.com/'
-else
-        Rails.application.config.session_store :cookie_store, key: '_gomojii'
-end
+# if Rails.env == "production"
+        # Rails.application.config.session_store :cookie_store, key: '_gomojii', domain: 'https://gomojii.com/'
+# else
+        Rails.application.config.session_store :cookie_store, key: '_gomojii', same_site: :none, secure: Rails.env.production? || Rails.env.development?
+# end
