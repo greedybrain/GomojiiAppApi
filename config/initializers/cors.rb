2b31fd58cd3218cf7b3beda813f66ab772d2e1e6
WhatYouCodingBoutWillis <whatyoucodingboutwillis@WhatYouCodingBoutWilliss-MacBook-Pro.local>
2020-10-19 20:10:46 -0400

    ...

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,7 +6,7 @@
 # Read more: https://github.com/cyu/rack-cors
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://gomojii.com'
     resource '*',
     headers: :any,
     methods: [:get, :post, :put, :patch, :delete, :options, :head],
