512916643cfefc1b4e18c947b8c94c95fa582b5b
greedybrain <greedybrain18@gmail.com>
2020-10-22 15:17:38 -0400

    ...

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,8 +6,8 @@
 # Read more: https://github.com/cyu/rack-cors
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://gomojii.com'
-    # origins 'http://localhost:3000'
+    # origins 'https://gomojii.com'
+    origins 'http://localhost:3000'
     resource '*',
     headers: :any,
     methods: [:get, :post, :put, :patch, :delete, :options, :head],
